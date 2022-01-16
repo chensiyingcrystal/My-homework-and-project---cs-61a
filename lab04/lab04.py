c3b7324a849901ce2d54bfee3e0de521c7883303
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-16 13:10:10 -0800

    Q1-Q3

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -1,56 +1,63 @@
 HW_SOURCE_FILE = __file__
 
-
+"""Q2: Summation"""
 def summation(n, term):
     """Return the sum of numbers 1 through n (including n) wíth term applied to each number.
-    Implement using recursion!
+    Implement using recursion!"""
+    if n == 0:
+        return 0
+    else:
+        return term(n) + summation(n - 1, term)
 
-    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
-    225
-    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
-    54
-    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
-    62
-    >>> # Do not use while/for loops!
-    >>> from construct_check import check
-    >>> # ban iteration
-    >>> check(HW_SOURCE_FILE, 'summation',
-    ...       ['While', 'For'])
-    True
-    """
-    assert n >= 1
-    "*** YOUR CODE HERE ***"
+# print(summation(5, lambda x: x * x * x)) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+# #     225
+# print(summation(9, lambda x: x + 1)) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+# #     54
+# print(summation(5, lambda x: 2**x)) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+# #     62
 
 
+"""Q3: Pascal's Triangle"""
 def pascal(row, column):
     """Returns the value of the item in Pascal's Triangle 
-    whose position is specified by row and column.
-    >>> pascal(0, 0)
-    1
-    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
-    0
-    >>> pascal(3, 2)	# Row 3 (1 3 3 1), Column 2
-    3
-    >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
-    6
-    """
-    "*** YOUR CODE HERE ***"
+    whose position is specified by row and column."""
+    if row == 0 and column == 0:
+        return 1
+    elif row == 0 and column > 0:
+        return 0
+    elif column == 0:
+        return 1
+    else:
+        return pascal(row - 1, column) + pascal(row - 1, column - 1)
+
+# print(pascal(0, 0))
+# #     1
+# print(pascal(0, 5))	# Empty entry; outside of Pascal's Triangle
+# #     0
+# print(pascal(3, 2))	# Row 3 (1 3 3 1), Column 2
+# #     3
+# print(pascal(4, 2))     # Row 4 (1 4 6 4 1), Column 2
+# #     6
 
 
+"""Q4: Insect Combinatorics"""
 def paths(m, n):
     """Return the number of paths from one corner of an
-    M by N grid to the opposite corner.
+    M by N grid to the opposite corner."""
+    
 
-    >>> paths(2, 2)
-    2
-    >>> paths(5, 7)
-    210
-    >>> paths(117, 1)
-    1
-    >>> paths(1, 157)
-    1
-    """
-    "*** YOUR CODE HERE ***"
+    return paths(m, n - 1) + paths(m - 1, n)
+
+
+# print(paths(2, 2))
+#     2
+# print(paths(5, 7))
+#     210
+# print(paths(117, 1))
+#     1
+# print(paths(1, 157))
+#     1
+
 
 
 def couple(s, t):
