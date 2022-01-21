8940e8d3b3e3ab0faeec2f2df140fc732e30f7b1
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-21 10:48:39 -0800

    Update hw04.py

--- hw04/hw04.py
+++ hw04/hw04.py
@@ -125,36 +125,110 @@ def balanced(m):
 #     # True
 
 
-def totals_tree(m):
-    """Return a tree representing the mobile with its total weight at the root.
+# Tree ADT
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch), 'branches must be trees'
+    return [label] + list(branches)
 
-    >>> t, u, v = examples()
-    >>> print_tree(totals_tree(t))
-    3
-      2
-      1
-    >>> print_tree(totals_tree(u))
-    6
-      1
-      5
-        3
-        2
-    >>> print_tree(totals_tree(v))
-    9
-      3
-        2
-        1
-      6
-        1
-        5
-          3
-          2
-    >>> from construct_check import check
-    >>> # checking for abstraction barrier violations by banning indexing
-    >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
-    True
+
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
     """
-    "*** YOUR CODE HERE ***"
+    return not branches(tree)
+
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
+
+
+def totals_tree(m):
+    """Return a tree representing the mobile with its total weight at the root."""
+    if is_planet(m):
+        return tree(total_weight(m))
+    if is_mobile(m):
+        return tree(total_weight(m), [totals_tree(end(left(m))), totals_tree(end(right(m)))])
+
+
+# t, u, v = examples()
+# print_tree(totals_tree(t))
+#     # 3
+#     #   2
+#     #   1
+# print_tree(totals_tree(u))
+#     # 6
+#     #   1
+#     #   5
+#     #     3
+#     #     2
+# print_tree(totals_tree(v))
+#     # 9
+#     #   3
+#     #     2
+#     #     1
+#     #   6
+#     #     1
+#     #     5
+#     #       3
+#     #       2
+# from construct_check import check
+# # checking for abstraction barrier violations by banning indexing
+# print(check(HW_SOURCE_FILE, 'totals_tree', ['Index']))
+#     # True
 
 
 def replace_loki_at_leaf(t, lokis_replacement):
@@ -328,73 +402,4 @@ def check_par():
     return r1, r2
 
 
-# Tree ADT
 
-def tree(label, branches=[]):
-    """Construct a tree with the given label value and a list of branches."""
-    for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
-    return [label] + list(branches)
-
-
-def label(tree):
-    """Return the label value of a tree."""
-    return tree[0]
-
-
-def branches(tree):
-    """Return the list of branches of the given tree."""
-    return tree[1:]
-
-
-def is_tree(tree):
-    """Returns True if the given tree is a tree, and False otherwise."""
-    if type(tree) != list or len(tree) < 1:
-        return False
-    for branch in branches(tree):
-        if not is_tree(branch):
-            return False
-    return True
-
-
-def is_leaf(tree):
-    """Returns True if the given tree's list of branches is empty, and False
-    otherwise.
-    """
-    return not branches(tree)
-
-
-def print_tree(t, indent=0):
-    """Print a representation of this tree in which each node is
-    indented by two spaces times its depth from the root.
-
-    >>> print_tree(tree(1))
-    1
-    >>> print_tree(tree(1, [tree(2)]))
-    1
-      2
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> print_tree(numbers)
-    1
-      2
-      3
-        4
-        5
-      6
-        7
-    """
-    print('  ' * indent + str(label(t)))
-    for b in branches(t):
-        print_tree(b, indent + 1)
-
-
-def copy_tree(t):
-    """Returns a copy of t. Only for testing purposes.
-
-    >>> t = tree(5)
-    >>> copy = copy_tree(t)
-    >>> t = tree(6)
-    >>> print_tree(copy)
-    5
-    """
-    return tree(label(t), [copy_tree(b) for b in branches(t)])
