29921e63697a667520a2e11828268a63e27e2f89
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-24 09:35:42 -0800

    Update lab06.py

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -44,28 +44,33 @@ def insert_items(lst, entry, elem):
 # from construct_check import check
 
 
+"""Q4: Count Occurrences"""
 def count_occurrences(t, n, x):
-    """Return the number of times that x appears in the first n elements of iterator t.
+    """Return the number of times that x appears in the first n elements of iterator t."""
+    i, times = 0, 0
+    while i < n:
+        if next(t) == x:
+            times += 1
+        i += 1
+    return times
 
-    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> count_occurrences(s, 10, 9)
-    3
-    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> count_occurrences(s2, 3, 10)
-    2
-    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
-    >>> count_occurrences(s, 1, 3)
-    1
-    >>> count_occurrences(s, 4, 2)
-    3
-    >>> next(s)
-    2
-    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
-    >>> count_occurrences(s2, 6, 6)
-    2
-    """
-    "*** YOUR CODE HERE ***"
 
+s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+print(count_occurrences(s, 10, 9))
+    # 3
+s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+print(count_occurrences(s2, 3, 10))
+    # 2
+s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+print(count_occurrences(s, 1, 3))
+    # 1
+print(count_occurrences(s, 4, 2))
+    # 3
+print(next(s))
+    # 2
+s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+print(count_occurrences(s2, 6, 6))
+    # 2
 
 def repeated(t, k):
     """Return the first value in iterator T that appears K times in a row.
