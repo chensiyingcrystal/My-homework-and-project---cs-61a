17ee96f27b5876233e5668bf99887b1861a9a296
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-23 22:11:20 -0800

    lab06 Q1

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -1,33 +1,47 @@
 HW_SOURCE_FILE = __file__
 
 
+"""Q2: Insert Items"""
 def insert_items(lst, entry, elem):
-    """Inserts elem into lst after each occurence of entry and then returns lst.
+    """Inserts elem into lst after each occurence of entry and then returns lst."""
+    i = 0
+    while i < len(lst):
+        if lst[i] == entry:
+            lst.insert(i + 1, elem)
+            i = i + 2
+        else:
+            i = i + 1
+    return lst
 
-    >>> test_lst = [1, 5, 8, 5, 2, 3]
-    >>> new_lst = insert_items(test_lst, 5, 7)
-    >>> new_lst
-    [1, 5, 7, 8, 5, 7, 2, 3]
-    >>> double_lst = [1, 2, 1, 2, 3, 3]
-    >>> double_lst = insert_items(double_lst, 3, 4)
-    >>> double_lst
-    [1, 2, 1, 2, 3, 4, 3, 4]
-    >>> large_lst = [1, 4, 8]
-    >>> large_lst2 = insert_items(large_lst, 4, 4)
-    >>> large_lst2
-    [1, 4, 4, 8]
-    >>> large_lst3 = insert_items(large_lst2, 4, 6)
-    >>> large_lst3
-    [1, 4, 6, 4, 6, 8]
-    >>> large_lst3 is large_lst
-    True
-    >>> # Ban creating new lists
-    >>> from construct_check import check
-    >>> check(HW_SOURCE_FILE, 'insert_items',
-    ...       ['List', 'ListComp', 'Slice'])
-    True
-    """
-    "*** YOUR CODE HERE ***"
+
+#Wrong solution:
+# def insert_items(lst, entry, elem):
+#     """Inserts elem into lst after each occurence of entry and then returns lst."""
+#     for i in range(0, len(lst)):       # Wrong reason: in the range function, len(lst) will not counted as lst changes
+#         if lst[i] == entry:
+#             lst.insert(i + 1, elem)    # Can't manipunate the "i"
+#     return lst
+
+
+# test_lst = [1, 5, 8, 5, 2, 3]
+# new_lst = insert_items(test_lst, 5, 7)
+# print(new_lst)
+#     # [1, 5, 7, 8, 5, 7, 2, 3]
+# double_lst = [1, 2, 1, 2, 3, 3]
+# double_lst = insert_items(double_lst, 3, 4)
+# print(double_lst)
+#     # [1, 2, 1, 2, 3, 4, 3, 4]
+# large_lst = [1, 4, 8]
+# large_lst2 = insert_items(large_lst, 4, 4)
+# print(large_lst2)
+#     # [1, 4, 4, 8]
+# large_lst3 = insert_items(large_lst2, 4, 6)
+# print(large_lst3)
+#     # [1, 4, 6, 4, 6, 8]
+# print(large_lst3 is large_lst)
+#     # True
+# # Ban creating new lists
+# from construct_check import check
 
 
 def count_occurrences(t, n, x):
